2268ef10a2d815c193f566839d9fbb51a910f7a1
xander04301 <110802453+xander04301@users.noreply.github.com>
2023-01-27 11:41:21 +0800

    ui

--- app/src/main/java/com/raTags/mobileapplication/RegistrationUser.java
+++ app/src/main/java/com/raTags/mobileapplication/RegistrationUser.java
@@ -3,12 +3,41 @@ package com.raTags.mobileapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistrationUser extends AppCompatActivity {
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText emailEditText;
+    private EditText phoneEditText;
+    private EditText passwordEditText;
+    private EditText confirmPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_user);
+
+                firstNameEditText = (EditText) findViewById(R.id.editFirstName);
+        lastNameEditText = (EditText) findViewById(R.id.editLastName);
+        emailEditText = (EditText) findViewById(R.id.editEmail);
+        phoneEditText = (EditText) findViewById(R.id.editCellphone);
+        passwordEditText = (EditText) findViewById(R.id.editPassword1);
+        confirmPasswordEditText = (EditText) findViewById(R.id.editPassword2);
+
+        Button signupButton = (Button) findViewById(R.id.btnSignup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = firstNameEditText.getText().toString();
+                String lastName = lastNameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String phone = phoneEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+            }
+        });
     }
 }
